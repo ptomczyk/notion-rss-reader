ecdc81ab68b3a0728fe21a5ef849401b6b22f8d7
P Tomczyk <bql@o2.pl>
2022-10-31 11:47:55 +0100

    remove dotenv

--- index.ts
+++ index.ts
@@ -2,9 +2,6 @@ import { getNewFeedItems } from '@/getNewFeedItems.ts'
 import { getFeedUrlList } from '@/getFeedUrlList.ts'
 import { addFeedItems } from '@/addFeedItems.ts'
 
-import dotenv from 'dotenv'
-dotenv.config()
-
 const feedUrlList = await getFeedUrlList()
 feedUrlList.forEach(async (feedUrl: string) => {
   if (feedUrl) {
